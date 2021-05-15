a78a84cef1c71edec27c96b56bc361d60951c7c8
mlcabral <mlcabral@riu.com>
2021-05-15 17:41:11 -0300

    init the project

---
+++ src/main/java/com/mariocabral/intercop/challenge/IntercopChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.mariocabral.intercop.challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntercopChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntercopChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mariocabral/intercop/challenge/IntercopChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mariocabral.intercop.challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntercopChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
