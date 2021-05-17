90163aaf3e471e94f5b98b78d854a565437299ff
mlcabral <mlcabral@riu.com>
2021-05-17 00:12:33 -0300

    add kpideclientes endpoint

--- src/main/java/com/mariocabral/intercop/challenge/controller/ClientController.java
+++ src/main/java/com/mariocabral/intercop/challenge/controller/ClientController.java
@@ -1,14 +1,12 @@
 package com.mariocabral.intercop.challenge.controller;
 
 import com.mariocabral.intercop.challenge.model.Client;
+import com.mariocabral.intercop.challenge.model.KPIClient;
 import com.mariocabral.intercop.challenge.service.ClientService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
@@ -22,6 +20,13 @@ public class ClientController {
     @ApiOperation(value = "Add new user", notes = "Return a user stored" )
     public Client newClient(@RequestBody(required = true) Client client){
         return clientService.newClient(client);
+    }
+
+
+    @GetMapping(value = "/kpideclientes")
+    @ApiOperation(value = "Stats from Client data", notes = "Return the avg and standard deviation of the ages froms clients" )
+    public KPIClient kpiClient(){
+        return clientService.kpiClient();
     }
 
 }
--- src/main/java/com/mariocabral/intercop/challenge/service/ClientService.java
+++ src/main/java/com/mariocabral/intercop/challenge/service/ClientService.java
@@ -1,9 +1,13 @@
 package com.mariocabral.intercop.challenge.service;
 
 import com.mariocabral.intercop.challenge.model.Client;
+import com.mariocabral.intercop.challenge.model.KPIClient;
 import com.mariocabral.intercop.challenge.repository.ClientRespository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 @Service
 public class ClientService {
@@ -13,5 +17,14 @@ public class ClientService {
 
     public Client newClient(Client client) {
         return clientRespository.save(client);
+    }
+
+    public KPIClient kpiClient() {
+        int avgOfAge = clientRespository.getAvgOfAge();
+        BigDecimal stddevOfAge = clientRespository.getStddevOfAge();
+        KPIClient result = new KPIClient();
+        result.setAvgAges(avgOfAge);
+        result.setStandardDeviationAges(stddevOfAge.setScale(2, RoundingMode.DOWN));
+        return result;
     }
 }
