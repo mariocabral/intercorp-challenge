d76764b862f7dd9b542abcbbd66977c9c7d5ffbc
mlcabral <mlcabral@riu.com>
2021-05-17 13:50:08 -0300

    improve implementation with more cases

--- src/main/java/com/mariocabral/intercop/challenge/controller/ApiExceptionHandler.java
+++ src/main/java/com/mariocabral/intercop/challenge/controller/ApiExceptionHandler.java
@@ -2,6 +2,7 @@ package com.mariocabral.intercop.challenge.controller;
 
 import com.mariocabral.intercop.challenge.exception.GenderCalculatorException;
 import com.mariocabral.intercop.challenge.exception.InvalidClientException;
+import com.mariocabral.intercop.challenge.exception.KPIException;
 import com.mariocabral.intercop.challenge.model.ErrorMessage;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -17,7 +18,7 @@ public class ApiExceptionHandler {
     @ResponseBody
     @ExceptionHandler(InvalidClientException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    ErrorMessage deviceNotFoundHandler(InvalidClientException ex) {
+    ErrorMessage invalidClientExceptionHandler(InvalidClientException ex) {
         ErrorMessage error = new ErrorMessage(new Date(),HttpStatus.BAD_REQUEST.value(), "Invalid Client", ex.getMessage());
         return error;
     }
@@ -25,8 +26,16 @@ public class ApiExceptionHandler {
     @ResponseBody
     @ExceptionHandler(GenderCalculatorException.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    ErrorMessage deviceValidationHandler(GenderCalculatorException ex) {
+    ErrorMessage genderCalculatorExceptionHandler(GenderCalculatorException ex) {
         ErrorMessage error = new ErrorMessage(new Date(),HttpStatus.BAD_REQUEST.value(), "Fail gender detector", ex.getMessage());
+        return error;
+    }
+
+    @ResponseBody
+    @ExceptionHandler(KPIException.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    ErrorMessage kPIExceptionHandler(KPIException ex) {
+        ErrorMessage error = new ErrorMessage(new Date(),HttpStatus.BAD_REQUEST.value(), "Fail KPI calculation process", ex.getMessage());
         return error;
     }
 }
